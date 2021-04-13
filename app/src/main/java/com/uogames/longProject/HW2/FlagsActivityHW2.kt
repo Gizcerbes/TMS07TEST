73044b4f5ca8d8c04460e5f17c9586f6cc3d0388
Gizcerbes <gizcerbes@outlook.com>
2021-04-14 00:36:30 +0300

    Add HW2

---
+++ app/src/main/java/com/uogames/longProject/HW2/FlagsActivityHW2.kt
@@ -0,0 +1,32 @@
+package com.uogames.longProject.HW2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.squareup.picasso.Picasso
+import com.uogames.longProject.R
+
+class FlagsActivityHW2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flags_hw2)
+
+        val flagArmenia = findViewById<ImageView>(R.id.hw2_flag_armenia)
+        val flagAustria = findViewById<ImageView>(R.id.hw2_flag_austria)
+        val flagBelgium = findViewById<ImageView>(R.id.hw2_flag_belgium)
+        val flagLatvia = findViewById<ImageView>(R.id.hw2_flag_latvia)
+        val flagDenmark = findViewById<ImageView>(R.id.hw2_flag_denmark)
+        val flagSpain = findViewById<ImageView>(R.id.hw2_flag_spain)
+
+        println(getString(R.string.Link_flag_armenia))
+
+        Picasso.get().load(getString(R.string.Link_flag_armenia)).into(flagArmenia)
+        Picasso.get().load(getString(R.string.Link_flag_austria)).into(flagAustria)
+        Picasso.get().load(getString(R.string.Link_flag_belgium)).into(flagBelgium)
+        Picasso.get().load(getString(R.string.Link_flag_latvia)).into(flagLatvia)
+        Picasso.get().load(getString(R.string.Link_flag_denmark)).into(flagDenmark)
+        Picasso.get().load(getString(R.string.Link_flag_spain)).into(flagSpain)
+
+    }
+}
--- app/src/main/java/com/uogames/longProject/MainActivity.kt
+++ app/src/main/java/com/uogames/longProject/MainActivity.kt
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.uogames.longProject.HW1.MainActivityHW1
+import com.uogames.longProject.HW2.MainActivityHW2
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         findViewById<Button>(R.id.button1).setOnClickListener { startActivity(Intent(this, MainActivityHW1::class.java)) }
+        findViewById<Button>(R.id.button2).setOnClickListener { startActivity(Intent(this, MainActivityHW2::class.java)) }
     }
 }
