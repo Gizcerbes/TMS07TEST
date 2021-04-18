841918d6d4d2f302ddda491b8ddd22b737c5a559
Gizcerbes <gizcerbes@outlook.com>
2021-04-18 15:20:17 +0300

    Create activity_main_hw3.xml

--- app/src/main/java/com/uogames/longProject/MainActivity.kt
+++ app/src/main/java/com/uogames/longProject/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 import com.uogames.longProject.HW1.MainActivityHW1
 import com.uogames.longProject.HW2.MainActivityHW2
+import com.uogames.longProject.HW3.MainActivityHW3
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +14,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         findViewById<Button>(R.id.button1).setOnClickListener { startActivity(Intent(this, MainActivityHW1::class.java)) }
         findViewById<Button>(R.id.button2).setOnClickListener { startActivity(Intent(this, MainActivityHW2::class.java)) }
+        findViewById<Button>(R.id.button3).setOnClickListener { startActivity(Intent(this, MainActivityHW3::class.java)) }
     }
 }
