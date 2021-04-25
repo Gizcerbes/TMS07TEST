26a9000caff248e9c1ce3c931f376f6cbcd64049
Gizcerbes <gizcerbes@outlook.com>
2021-04-25 19:40:32 +0300

    create activity_main_hw5.xml

---
+++ app/src/main/java/com/uogames/longProject/HW5/MainActivityHW5.kt
@@ -0,0 +1,12 @@
+package com.uogames.longProject.HW5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.uogames.longProject.R
+
+class MainActivityHW5 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_hw5)
+    }
+}
