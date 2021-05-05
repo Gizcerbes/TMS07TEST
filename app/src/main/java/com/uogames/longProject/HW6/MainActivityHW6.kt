773e34ad2f70a3efe6fd903d1369ddc3833e9205
Gizcerbes <gizcerbes@outlook.com>
2021-05-05 19:40:02 +0300

    ADD Navigation

---
+++ app/src/main/java/com/uogames/longProject/HW6/MainActivityHW6.kt
@@ -0,0 +1,11 @@
+package com.uogames.longProject.HW6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityHW6 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_hw6)
+    }
+}
